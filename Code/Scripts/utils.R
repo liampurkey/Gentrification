6644551d41a81f56f19aa277bdc7d2442565983a
Liam Purkey <lpurkey@macalester.edu>
2021-04-28 13:41:12 -0500

    Update WAC and ACS cleaning files

--- Code/Scripts/utils.R
+++ Code/Scripts/utils.R
@@ -3,12 +3,15 @@
 ##Cleaning Functions
 
 balance.panel = function(group_id, panel_length, data) {
+    
+  #The balance.panel function takes in a balanced panel where missing unit-time pairs are not explicit.
+  #It returns a balanced panel by dropping units with missing time observations.
   
   out_data = data %>%
     dplyr::group_by(!!as.name(group_id)) %>%
     dplyr::filter(dplyr::n() == panel_length) %>%
     dplyr::ungroup()
-  
+    
   return(out_data)
   
 }

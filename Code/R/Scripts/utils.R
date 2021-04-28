7c3a3e04f675e4a2e175dc659c4811d97981650b
Liam Purkey <lpurkey@macalester.edu>
2021-04-27 23:12:23 -0500

    Update utils

--- Code/R/Scripts/utils.R
+++ Code/R/Scripts/utils.R
@@ -2,8 +2,61 @@
 
 ##Cleaning Functions
 
+normalize = function(data, variables, total, prefix = NULL, drop = FALSE) {
+  
+  #Normalize takes in a dataset, a set of variables to normalize, and a variable to normalize by,
+  #and returns a dataset with normalized variables. The user has the option to provide their own
+  #prefix with the prefix argument and can also drop the original variables with the drop argument.
+  
+  out_data = data 
+  
+  if (is.null(newname)) {
+    
+    for (i in 1:length(variables)) {
+      
+      var = variables[i]
+      varname = paste("p", var, sep = "_")
+      out_data = out_data %>% 
+        dplyr::mutate(!!varname := !!as.name(var)/!!as.name(total)) 
+      
+      if (isTRUE(drop)) {
+        out_data = out_data %>%
+          dyplr::select(-!!as.name(var))
+      }
+      
+    }
+    
+  } else {
+    
+    for (i in 1:length(variables)) {
+      
+      var = variables[i]
+      varname = paste(prefix, var, sep = "_")
+      out_data = out_data %>% 
+        dplyr::mutate(!!varname := !!as.name(var)/!!as.name(total)) 
+      
+      if (isTRUE(drop)) {
+        out_data = out_data %>%
+          dplyr::select(-!!as.name(var))
+      }
+      
+    }
+    
+  }
+  
+  return(out_data)
+  
+}
+
 ##Spatial Functions
 
 st_erase <- function(x, y) {
   sf::st_difference(x, sf::st_union(y))
 }
+
+clean_units <- function(x){
+  attr(x,"units") <- NULL
+  class(x) <- setdiff(class(x),"units")
+  x
+}
+

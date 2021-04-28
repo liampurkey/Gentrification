9075f1aed4a6589ec67d643aa2a2fc28eb3d64cc
Liam Purkey <lpurkey@macalester.edu>
2021-04-28 13:02:38 -0500

    Remove R directory and update WAC cleaning

---
+++ Code/Scripts/utils.R
@@ -0,0 +1,27 @@
+#Readme: This file contains functions for my research on gentrification and the racial composition of employment
+
+##Cleaning Functions
+
+balance.panel = function(group_id, panel_length, data) {
+  
+  out_data = data %>%
+    dplyr::group_by(!!as.name(group_id)) %>%
+    dplyr::filter(dplyr::n() == panel_length) %>%
+    dplyr::ungroup()
+  
+  return(out_data)
+  
+}
+
+##Spatial Functions
+
+st.erase <- function(x, y) {
+  sf::st_difference(x, sf::st_union(y))
+}
+
+clean.units <- function(x){
+  attr(x,"units") <- NULL
+  class(x) <- setdiff(class(x),"units")
+  x
+}
+
